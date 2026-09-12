9143b2d93bcf5b99e2cc4336ba0c6e6ef7cccb89
syedtahirmehboob0-glitch <syedtahirmehboob0@gmail.com>
2026-09-12 21:31:20 +0300

    Fix Unity Android build entry point

--- Assets/Editor/AndroidBuild.cs
+++ Assets/Editor/AndroidBuild.cs
@@ -1,30 +1,44 @@
 #if UNITY_EDITOR
 using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.IO;
 
 public static class AndroidBuild
 {
-    public static void BuildAPK(){
+    public static void Build()
+    {
+        BuildAPK();
+    }
+
+    public static void BuildAPK()
+    {
         StreetFootballBootstrap.BuildDemo();
         Directory.CreateDirectory("Builds");
-        PlayerSettings.productName="Pakistan Street Football";
-        PlayerSettings.companyName="Pakistan Street Football Studio";
-        PlayerSettings.applicationIdentifier="com.pakistan.streetfootball";
-        PlayerSettings.bundleVersion="0.1.0";
-        PlayerSettings.Android.bundleVersionCode=1;
-        PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android,ScriptingImplementation.IL2CPP);
-        PlayerSettings.Android.targetArchitectures=AndroidArchitecture.ARM64;
-        PlayerSettings.Android.minSdkVersion=AndroidSdkVersions.AndroidApiLevel26;
-        PlayerSettings.Android.targetSdkVersion=AndroidSdkVersions.AndroidApiLevel35;
-        EditorUserBuildSettings.buildAppBundle=false;
-        EditorUserBuildSettings.development=false;
-        var opts=new BuildPlayerOptions{scenesToBuild=new[]{"Assets/Scenes/StreetMatch.unity"},locationPathName="Builds/PakistanStreetFootball.apk",target=BuildTarget.Android,options=BuildOptions.None};
-        var report=BuildPipeline.BuildPlayer(opts);
-        if(report.summary.result!=UnityEditor.Build.Reporting.BuildResult.Succeeded) throw new System.Exception("Android build failed: "+report.summary.result);
-        Debug.Log("APK READY: "+Path.GetFullPath(opts.locationPathName));
+        PlayerSettings.productName = "Pakistan Street Football";
+        PlayerSettings.companyName = "Pakistan Street Football Studio";
+        PlayerSettings.applicationIdentifier = "com.pakistan.streetfootball";
+        PlayerSettings.bundleVersion = "0.1.0";
+        PlayerSettings.Android.bundleVersionCode = 1;
+        PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+        PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARM64;
+        PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel26;
+        PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel35;
+        EditorUserBuildSettings.buildAppBundle = false;
+        EditorUserBuildSettings.development = false;
+
+        var opts = new BuildPlayerOptions
+        {
+            scenesToBuild = new[] { "Assets/Scenes/StreetMatch.unity" },
+            locationPathName = "Builds/PakistanStreetFootball.apk",
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        var report = BuildPipeline.BuildPlayer(opts);
+        if (report.summary.result != UnityEditor.Build.Reporting.BuildResult.Succeeded)
+            throw new System.Exception("Android build failed: " + report.summary.result);
+
+        Debug.Log("APK READY: " + Path.GetFullPath(opts.locationPathName));
     }
 }
 #endif
